6f219f98a3fdfef5059222d7c455240d81fa2c99
navodya-ui <tharaninawodya9@gmail.com>
2026-02-11 11:14:37 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/oceanview/reservation/OceanViewReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.oceanview.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanViewReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanViewReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oceanview/reservation/OceanViewReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceanview.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanViewReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
